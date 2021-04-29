3dcd6524d0505d29a4b9e5e085930f9e43c70bb4
BenJones <71987634+BenJonesMakers@users.noreply.github.com>
2021-04-29 16:17:29 +0100

    Added a speech test on fastest lap

--- src/components/FastestLap.js
+++ src/components/FastestLap.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import getRealName from '../helpers/TransponderLookup';
+import speech from 'speech-js';
 
 const FastestLap = (props) => {
   const { transponder, lapTime = 0 } = props.fastestLap;
+
+  useEffect(() => {
+    speech.synthesis(getRealName(transponder) + ', just did an, ' + lapTime.toFixed(3), 'en-US')
+  }, [transponder, lapTime]);
+
 
   return (
     <>
